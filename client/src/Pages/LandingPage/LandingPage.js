b632410a8c0ae4497e1cd659b8bd089ee96ce60e
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-02-08 12:27:46 +0100

    create react package

---
+++ client/src/Pages/LandingPage/LandingPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './LandingPage.css'
+
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="tab">
+        <div className="brand">
+          <h3>The <span>Neuro-Lab</span></h3>
+        </div>
+        <div className="nav">
+          <h3>Research</h3>
+          <h3>Techniques</h3>
+          <h3>About</h3>
+        </div>
+      </div>
+      <div className="loaders">
+        
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
