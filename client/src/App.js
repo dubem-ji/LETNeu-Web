c217482f1f70f5e4aca8e8d789eae0b9b8675c09
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-03-14 12:52:34 +0100

    create and import research.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Homepage from './Pages/Homepage';
+import Research from './Pages/Research/Research';
 
 function App() {
   return (
     <div className="App">
       <Homepage />
+      <Research />
     </div>
   );
 }
