b0d9518bf309a99a7d5dbe7f127b3c2ccc44b343
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-03-14 14:19:06 +0100

    update research.js

--- client/src/Pages/Research/Research.js
+++ client/src/Pages/Research/Research.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import p2 from '../../Components/images/pub-cards/p2.JPG';
+import p3 from '../../Components/images/pub-cards/p3.JPG';
+import p4 from '../../Components/images/pub-cards/p4.JPG';
+import p5 from '../../Components/images/pub-cards/p5.JPG';
+
 const Research = () => {
     return (
-        <div>
-            <Link to='/'>Home</Link>
+        <div className='publications'>
+            <div className="scroll">
+                <div className="cards">
+                    <div className="item" id='item-one'>
+                    </div>
+                    <div className="item" id='item-two'>
+                        <img src={p2} alt="" />
+                    </div>
+                    <div className="item" id='item-three'>
+                        <img src={p3} alt="" />
+                    </div>
+                    <div className="item" id='item-four'>
+                        <img src={p4} alt="" />
+                    </div>
+                    <div className="item" id="item-five">
+                        <img src={p5} alt="" />
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="item" id='item-one'>
+                        <img src={p2} alt="" />
+                    </div>
+                    <div className="item" id='item-two'></div>
+                    <div className="item" id='item-three'></div>
+                    <div className="item" id='item-four'></div>
+                    <div className="item" id="item-five"></div>
+                </div>
+            </div>
+            <div className="navs">
+                <div className="links">
+                    <h4><Link to='/'>HOME</Link></h4>
+                    <h4><Link to='/project'>PROJECTS</Link></h4>
+                </div>
+                <div className="display-options">
+                    <h4>OVERVIEW</h4>
+                    <h4>SELECTED</h4>
+                </div>
+            </div>
         </div>
     );
 };
