242114424da87619749a4dc2819c86ae92d528b4
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-03-14 13:03:25 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Homepage from './Pages/Homepage';
 import Research from './Pages/Research/Research';
+import Project from './Pages/Projects/Project';
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
-      <Research />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/research" element={<Research />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
     </div>
   );
 }
