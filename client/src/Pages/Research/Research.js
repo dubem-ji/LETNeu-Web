c2776e007ceb5fdb2e8991c43681132c1e9ace88
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-03-14 13:03:17 +0100

    Update Research.js

--- client/src/Pages/Research/Research.js
+++ client/src/Pages/Research/Research.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Research = () => {
-  return (
-    <div>Research</div>
-  )
-}
+    return (
+        <div>
+            <Link to='/'>Home</Link>
+        </div>
+    );
+};
 
-export default Research
+export default Research;
