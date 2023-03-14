83b05a263745409526c6220026c056c463085875
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-03-14 13:03:20 +0100

    Update Homepage.js

--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Homepage = () => {
     return (
         <div className='homepage'>
-          
+        <Link to='/research'>Research</Link>
+        <Link to='/project'>Project</Link>
         </div>
     );
 };
